cd5d53bf92fa42b7b79c3a92b18a4c8e006474e0
ahilendra9singh8 <shailendra9.singh8@gmail.com>
2025-05-13 17:21:38 +0530

    first commit

---
+++ src/main/java/dms/ApogeeDmsApplication.java
@@ -0,0 +1,13 @@
+package dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApogeeDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApogeeDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dms/ApogeeDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApogeeDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
